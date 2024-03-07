95d6c5b9861b3ae78622066c6d74c3434c7bf0d6
biellopes00 <gabriellopes031997@hotmail.com>
2024-03-07 12:03:14 -0300

    project

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./ui/button";
+
+const MainNav = () => {
+ return(
+    <Button
+        variant="ghost"
+        className="font-bold hover:text-green-500 hover:bg-white"
+    >
+
+    </Button>
+ )
+}
+
+export default MainNav;
