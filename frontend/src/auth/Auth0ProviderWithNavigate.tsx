e9dc21979dbd279a3b2049531768579f14abac6f
biellopes00 <gabriellopes031997@hotmail.com>
2024-03-08 13:40:52 -0300

    Creating auth

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,24 +1,23 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { AppState, Auth0Provider, User, useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
     children: React.ReactNode;
 }
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
- const { createUser } = useCreateMyUser();
-
- const domain = import.meta.env.VITE_AUTH0_DOMAIN;
- const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
- const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+    const navigate = useNavigate();
+    const { getAccessTokenSilently } = useAuth0();
+    const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+    const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+    const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
 
  if(!domain || !clientId || !redirectUri) {
     throw new Error("Unable to initialise auth");
  }
-const onRedirectCallback = (appState?: AppState, user?: User)=>{
-    if(user?.sub && user?.email) {
-        createUser({ auth0Id: user.sub, email: user.email })
-    }
+const onRedirectCallback = async (appState?: AppState, user?: User)=>{
+    const token = await getAccessTokenSilently();
+    navigate("/auth-callback")
 }
 
  return (
