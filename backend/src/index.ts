fbd125ed0a87ef46d478b69aabc07ace6e33d008
biellopes00 <gabriellopes031997@hotmail.com>
2024-03-08 11:09:34 -0300

    Creating controllers and routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,17 +2,16 @@ import express, { Request, Response} from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute"
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
     .then( ()=> console.log("Connected to database!"));
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get("/test" , async (req: Request, res: Response) =>{
-    res.json({message: "hello!"})
-})
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, ()=> {
     console.log("Server started on localhost:7000")
